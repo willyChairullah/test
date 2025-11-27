7a8faaca3d30d6d7f7135c06715796e4d66ca843
frmnxhken <frmnxhken@gmail.com>
2025-11-27 14:59:57 +0700

    Register Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,22 @@
 <?php
 
+use App\Http\Middleware\AuthorAccess;
+use App\Http\Middleware\EnsureClassroomAccess;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            "joined" => EnsureClassroomAccess::class,
+            "author" => AuthorAccess::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
