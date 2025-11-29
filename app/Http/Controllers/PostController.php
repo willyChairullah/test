3c9234373ed5f8e09c1ff082b19f5412b8353225
OyyHuss <mishbahus30@gmail.com>
2025-11-29 12:31:08 +0700

    pull prubahan

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,34 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function createAssignment($id)
+    {
+        return view("assignment.create", compact("id"));
+    }
+
+    public function storeAssignment(Request $request, $id)
+    {
+        $request->validate([
+            "title" => "required|string|max:255",
+            "content" => "nullable|string",
+            "due" => "required",
+            "files.*" => "nullable|file|max:10240",
+        ]);
+
+        $classroom = Classroom::with('members')->find($id);
+        $post = Post::where('id', 1);
+
+        $post = Post::create([
+            "user_id" => Auth::user()->id,
+            "classroom_id" => $classroom->id,
+            "title" => $request->title,
+            "content" => $request->content,
+            "due" => $request->due,
+            "type" => "assignment",
+        ]);
+
+        $this->uploadPostFiles($request, $post);
+        $this->generateSubmissions($post, $classroom);
+        return redirect()->back()->with(["success" => "Tugas berhasil diupload!"]);
+    }
 }
