c032e78ea2c9f2085b962bf39015e1c8a2c22d4e
vyarsnd <navyarisandii@gmail.com>
2025-11-30 13:27:08 +0700

    LMS-125 Get member list

--- app/Http/Controllers/MemberClassController.php
+++ app/Http/Controllers/MemberClassController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Classroom;
+use App\Models\MemberClass;
+use App\Models\Submission;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MemberClassController extends Controller
 {
-    //
+    public function index($id)
+    {
+        $userId = Auth::user()->id;
+        $isAuthor = $userId === Classroom::find($id)->user_id;
+        $author = Classroom::with("user")->where("id", $id)->first();
+        $members = MemberClass::with("user")->where("classroom_id", $id)->get();
+        return view("classroom.member", compact("members", "author", "isAuthor", "id"));
+    }
 }
