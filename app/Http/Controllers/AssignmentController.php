25fda0ed6c85f8fdbadb5fbf7fc819c083bc3687
OyyHuss <mishbahus30@gmail.com>
2025-11-30 16:53:47 +0700

    LMS-132 List Assignment

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Classroom;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AssignmentController extends Controller
 {
-    //
+    public function assignmentClass($id)
+    {
+        $userId = Auth::user()->id;
+        $isAuthor = $userId === Classroom::find($id)->user_id;
+        $classroom = Classroom::with(['posts' => function ($q) {
+            $q->where('type', 'assignment');
+        }])->findOrFail($id);
+
+        foreach ($classroom->posts as $post) {
+            $submitted = $post->submission()->whereIn('status', ['done', 'graded'])->count();
+            $pending = $post->submission()->where('status', 'pending')->count();
+
+            $post->submitted = $submitted;
+            $post->pending = $pending;
+        }
+
+        return view("assignment.index", compact("classroom", "id", "isAuthor"));
+    }
 }
